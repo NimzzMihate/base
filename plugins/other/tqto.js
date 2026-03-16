8e22bbb1327b9a9ca01c472cb30d51c3e18fb80b
NimzzMihate <nimzztes@gmail.com>
2026-03-16 12:57:24 +0700

    upload: plugins/other/tqto.js

---
+++ plugins/other/tqto.js
@@ -0,0 +1,25 @@
+const handler = async (m, { reply }) => {
+const tqto = `*THANKS TO*
+
+• Allah SWT
+• Kedua orang tua
+• Penyedia base script (Nimzz)
+• Jarr for code
+• Developer bot whatsApp (tqto share code and knowledge)
+• WhiskeySockets (Baileys)
+• Para pengguna base
+• Semua yang udah support
+• Yang udah bantu testing
+• Yang laporin bug
+• Claude 
+• Deepseek 
+• GPT
+• Gemini
+
+✨ Makasih banyak!`;
+
+  reply(tqto);
+};
+
+handler.command = ["tqto", "thanksto"];
+export default handler;
